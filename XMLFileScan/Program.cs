7e4831312f7f6ca6570e3046cfb10efdf0330a2b
lester88a <lester88a@gmail.com>
2015-12-30 16:09:18 -0500

    Dec 30,2015 Sovle the method of verifingResolutionList

--- XMLFileScan/Program.cs
+++ XMLFileScan/Program.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace XMLFileScan
 {
@@ -22,6 +25,59 @@ namespace XMLFileScan
                 VerifyResolutionFile vfFile = new VerifyResolutionFile();
                 vfFile.VerifyResolutionFileMethod(); //call the method
 
+                //XmlDocument doc = new XmlDocument();
+                //doc.Load(@"D:\XML\Sample3.xml");
+
+                //XmlNamespaceManager nameSpaceManager = new XmlNamespaceManager(doc.NameTable);
+
+                //nameSpaceManager.AddNamespace()
+
+                //XNamespace tns = "http://xmlschema.tmi.telus.com/srv/RMO/ResourceMgmt/EquipmentRepairVendorManagementSvcRequestResponse_v1";
+                //XNamespace ervmt = "http://xmlschema.tmi.telus.com/xsd/Resource/Resource/EquipmentRepairVendorManagement_v1";
+                //doc.CreateElement(tns.NamespaceName, "repairOrderResolution");
+                //doc.CreateElement(ervmt.NamespaceName + "repairID");
+                //doc.CreateElement(ervmt.NamespaceName + "repairResolutionList");
+                
+                //doc.CreateElement("tns", "repairOrderResolution", tns.NamespaceName);
+                //doc.CreateElement("ervmt", "repairID", ervmt.NamespaceName);
+                //doc.CreateElement("tns", "repairResolutionList", ervmt.NamespaceName);
+
+                ////doc.Load(@"D:\XML\Sample1.xml".Substring(@"D:\XML\Sample1.xml".IndexOf(Environment.NewLine)));
+                ////doc.LoadXml(@"D:\XML\Sample1.xml".Substring(@"D:\XML\Sample1.xml".IndexOf(Environment.NewLine)));
+                //XPathDocument docNav = new XPathDocument(new StringReader(@"D:\XML\Sample1.xml"));
+                //XPathNavigator navigator = docNav.CreateNavigator();
+                //XPathNodeIterator NodeIter = navigator.Select("/bigBook/books/book");
+                //foreach (XPathNavigator selectedNode in NodeIter)
+                //{
+                //    var a = "<root>" + selectedNode.InnerXml + "</root>";
+                //    var x = XDocument.Parse(a);
+                //    Console.WriteLine(x.Root.Element("bookName").Value);
+                //    if (x.Root.Element("bookID") != null)
+                //    {
+                //        Console.WriteLine(x.Root.Element("bookID").Value);
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("NOOOOOOO");
+                //    }
+                //}
+
+                //foreach (XmlElement e in doc.SelectNodes("//tns:repairOrderResolution")) 
+                //{
+                //    string bookID = e.SelectSingleNode("ervmt:repairID").InnerText;
+                //    //string bookName = e.SelectSingleNode("ervmt:repairResolutionList").InnerText;
+                //    Console.WriteLine(bookID);
+                //    //Console.WriteLine("{0}: {1}", bookID, bookName);
+                //}
+
+                //foreach (XmlElement e in doc.SelectNodes("//*[local-name() = 'repairOrderResolution']"))
+                //{
+                //    string bookID = e.SelectSingleNode("*[local-name() = 'repairID']").InnerText;
+                //    string bookName = e.SelectSingleNode("*[local-name() = 'repairResolutionList']").InnerText;
+
+                //    Console.WriteLine("{0}: {1}\n\n", bookID, bookName);
+                //}
+   
             }
             catch (Exception e)
             {
--- XMLFileScan/VerifyResolutionList.cs
+++ XMLFileScan/VerifyResolutionList.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace XMLFileScan
 {
@@ -23,42 +24,69 @@ namespace XMLFileScan
         {
             XmlReader reader = XmlReader.Create(fileName);
 
+            //elements
+            string repairOrderResolution = null;
+            string repairResolutionList = null;
+            string repairResolutionCode = null;
+
+            //declare a repairIDCount variable
+            int repairOrderResolutionCount = 0;
+            int repairResolutionListCount = 0;
+
             //verify the repairResolutionList
             reader.MoveToContent();
             while (reader.Read())
             {
-                
+
                 //find the specific tag (ervmt:repairResolutionCode) of the resolution XML file
-                string repairResolutionCode = null;
-                //if (reader.NodeType == XmlNodeType.Element && reader.Name == "tns:repairOrderResolution>")
+
+                //if (reader.NodeType == XmlNodeType.Element && reader.Name == "tns:repairOrderResolution")
                 //{
-                //    //while (reader.Read())
-                //    //{
+                //    while (reader.Read())
+                //    {
                 //        if (reader.NodeType == XmlNodeType.Element && reader.Name == "ervmt:repairResolutionList")
                 //        {
-                //            //while (reader.Read())
-                //            //{
+                //            while (reader.Read())
+                //            {
                 //                if (reader.NodeType == XmlNodeType.Element && reader.Name == "ervmt:repairResolutionCode")
                 //                {
+                //                    repairResolutionCode = reader.ReadString();
                 //                    if (repairResolutionCode == "")
                 //                    {
-                //                        Console.WriteLine(" has empty repairResolutionCode!!!");
-                //                    }
-                //                    else
-                //                    {
-                //                        Console.WriteLine("OK");
+                //                        Console.WriteLine(" has empty repairResolutionCode. Failed!!!");
                 //                    }
                 //                }
-                //            //}
+                //            }
                 //        }
+                //    }
+                //}
+
+
+
+                //if (reader.NodeType == XmlNodeType.Element && reader.Name == "tns:repairOrderResolution" )
+                //{
+                //    repairOrderResolutionCount++;
+                //    Console.WriteLine("repairOrderResolution: {0}", repairOrderResolutionCount);
+
+                //    //while (reader.Read())
+                //    //{
+                //    //    if (reader.NodeType == XmlNodeType.Element && reader.Name.Contains("ervmt:repairResolutionList"))
+                //    //    {
+                //    //        if (reader.IsEmptyElement)
+                //    //        {
+                //    //            Console.WriteLine("NOOOOO");
+                //    //        }
+                //    //    }
                 //    //}
 
                 //}
-                if (reader.NodeType == XmlNodeType.Element && reader.Name == "tns:repairOrderResolution")
-                {
-                    Console.WriteLine("yes");
+                //if (reader.NodeType == XmlNodeType.Element && reader.Name.Contains("ervmt:repairResolutionList"))
+                //{
+                //    repairResolutionListCount++;
+                //    Console.WriteLine("repairResolutionList: ------- {0} -------", repairResolutionListCount);
                     
-                }
+                    
+                //}
                 if (reader.NodeType == XmlNodeType.Element && reader.Name == "ervmt:repairResolutionCode")
                 {
                     repairResolutionCode = reader.ReadString();
@@ -67,24 +95,34 @@ namespace XMLFileScan
                         Console.WriteLine("The repairResolutionCode is empty. Failed!!!\n");
                     }
 
-
                 }
-                //if (reader.NodeType == XmlNodeType.Element && reader.Name.Contains("ervmt:repairResolutionList"))
-                //{
-                //    Console.WriteLine("++++++++++++++++");
-                //}
-
-                //if (reader.NodeType == XmlNodeType.Element && reader.Name == "ervmt:repairResolutionCode")
-                //{
-                //    repairResolutionCode = reader.ReadString();
-                //    if (repairResolutionCode == "")
-                //    {
-                //        //throw new Exception("The repairResolutionCode is empty!!\n");
-                //        Console.WriteLine("The repairResolutionCode is empty. Failed!!!\n");
-                //    }
-                    
-                //}
                 
+                
+                
+            }
+            //Console.WriteLine("Total repairOrderResolutionCount: {0}, Total repairResolutionListCount: {1}", repairOrderResolutionCount, repairResolutionListCount);
+
+
+            //This is verify the missling whole part of the repairResolutionList tag
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fileName);
+
+            foreach (XmlElement e in doc.SelectNodes("//*[local-name() = 'repairOrderResolution']"))
+            {
+                try
+                {
+                    string bookID = e.SelectSingleNode("*[local-name() = 'repairID']").InnerText;
+                    string bookName = e.SelectSingleNode("*[local-name() = 'repairResolutionList']").InnerText;
+                    
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Missing the whole part of the repairResolutionList tag. Failed!!!"); ;
+                }
+                
+
+                //Console.WriteLine("{0}: {1}\n\n", bookID, bookName);
             }
         }
     }
